7832838c741156b0877ad3428061377919786694
Anshula <kanshula@gmail.com>
2024-01-08 19:54:06 +0530

    codes

---
+++ 6) Arrays - Easy/2) Second lardest element in an array/Brute force.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+
+def getElements(arr, n):
+    if n == 0 or n == 1:
+        print(-1, -1)  # edge case when only one element is present in array
+    arr.sort()
+    small = arr[1]
+    large = arr[n-2]
+    print("Second smallest is", small)
+    print("Second largest is", large)
+
+
+
+
+if __name__ == '__main__':
+    arr = [1, 2, 4, 6, 7, 5]
+    n = len(arr)
+    getElements(arr, n)
+
+
+# In[ ]:
+
+
+
+
